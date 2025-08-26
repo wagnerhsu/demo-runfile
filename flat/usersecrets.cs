3fd48906414c517f0fce07784d3b482f22923cf7
Damian Edwards <damian@damianedwards.com>
2025-08-26 15:23:10 -0700

    Update usersecrets.cs

--- flat/usersecrets.cs
+++ flat/usersecrets.cs
@@ -1,16 +1,59 @@
 #:package Microsoft.Extensions.Configuration.UserSecrets@10.0.0-*
-#:property UserSecretsId=runfile-flat-usersecrets-12345
+#:property LangVersion=preview
 
 using System.Reflection;
+using System.Security.Cryptography;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 var key = args.Length > 0 ? args[0] : "MySecret";
 
 var configuration = new ConfigurationBuilder()
-    .AddUserSecrets(Assembly.GetExecutingAssembly())
+    .AddUserSecrets()
     .Build();
 
 Console.WriteLine(configuration[key] ?? $"""
     Value for secret '{key}' was not found. Run the following command to set one:
-    dotnet user-secrets set --id runfile-flat-usersecrets-12345 {key} "This is a secret!!"
+    dotnet user-secrets set --id {IConfigurationBuilder.GetDefaultUserSecretsId()} {key} "This is a secret!!"
     """);
+
+static class UserSecretsExtensions
+{
+    extension(IConfigurationBuilder builder)
+    {
+        /// <summary>
+        /// Gets the implicit user secrets ID.
+        /// </summary>
+        /// <remarks>
+        /// This method defaults the user secrets ID to:
+        /// - the value of the <c>UserSecretsIdAttribute</c> attribute in the entry assembly if present
+        /// - for file-based apps, a value derived from the app file path
+        /// - otherwise, the string "global"
+        /// </remarks>
+        public static string GetDefaultUserSecretsId()
+        {
+            var userSecretsId = Assembly.GetEntryAssembly()?.GetCustomAttribute<UserSecretsIdAttribute>()?.UserSecretsId;
+            if (userSecretsId is not null)
+            {
+                // Use the UserSecretsId from the attribute
+                return userSecretsId;
+            }
+
+            if (AppContext.GetData("EntryPointFilePath") is string entryPointFilePath)
+            {
+                // Use the file path to generate a unique ID for user secrets
+                return Convert.ToHexString(SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(entryPointFilePath)));
+            }
+
+            return "global";
+        }
+
+        /// <summary>
+        /// Adds user secrets configuration source to <see cref="IConfigurationBuilder"/> based on the user secrets ID returned by <see cref="GetDefaultUserSecretsId"/>.
+        /// </summary>
+        public IConfigurationBuilder AddUserSecrets()
+        {
+            return builder.AddUserSecrets(GetDefaultUserSecretsId());
+        }
+    }
+}
